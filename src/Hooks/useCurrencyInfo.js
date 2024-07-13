9870b58953486fc12209ce245d6a92979d971de9
fazal199 <fazalhussain1441@gmail.com>
2024-07-13 17:20:16 +0530

    updated the api url

--- src/Hooks/useCurrencyInfo.js
+++ src/Hooks/useCurrencyInfo.js
@@ -1,17 +1,17 @@
-import { useEffect,useState } from "react"
+import { useEffect, useState } from "react"
 
-const useCurrencyInfo = (currency)=>{
+const useCurrencyInfo = (currency) => {
 
-    let [data,setData] = useState([]);
-        
-     useEffect(()=>{
-        fetch(`https://cdn.jsdelivr.net/gh/fawazahmed0/currency-api@1/latest/currencies/${currency}.json`)
-        .then(response => response.json()).then(data => setData(data[currency]));
-     },[currency])
+   let [data, setData] = useState([]);
 
-     console.log(data);
-     return data;
-     
+   useEffect(() => {
+      fetch(`https://latest.currency-api.pages.dev/v1/currencies/${currency}.json`)
+         .then(response => response.json()).then(data => setData(data[currency]));
+   }, [currency])
+
+   console.log(data);
+   return data;
+
 }
 
 export default useCurrencyInfo;
